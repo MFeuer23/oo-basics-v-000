653ae93474d4a3e81399d2b3674b9f76dc09b525
Mallory Feuer <mallory.feuer@gmail.com>
2018-02-13 14:18:58 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,5 +1,11 @@
 # Make your shoe class here!
 class Shoe
+  def initialize(brand)
+    @brand = brand
+  end
   
+  attr_reader :brand
+  
+  attr_accessor :color, :size, :material, :condition
   
 end
